3603a4d692892c58c03b6f8b2ea31b5196c47656
motemen <motemen@gmail.com>
2013-12-02 13:48:40 +0900

    use class

--- diff-sticky-filename/diff-sticky-filename.js
+++ diff-sticky-filename/diff-sticky-filename.js
@@ -4,26 +4,29 @@
         return;
     }
 
+    $('<style>').text([
+        '.diff-view .file.userscript-dsf-sticky {',
+        '   padding-top: 44px;',
+        '}',
+        '.diff-view .file.userscript-dsf-sticky .meta {',
+        '   position: fixed;',
+        '   top: 5px;',
+        '   border-top: 1px solid #d8d8d8;',
+        '}'
+    ].join('\n')).appendTo(document.querySelector('head'));
+
     $(window).on('scroll', function () {
         var scrollTop = $(this).scrollTop();
         var found = false;
         $($('.diff-view .file').toArray().reverse()).each(function () {
             var $this = $(this);
             if (!found && $this.position().top <= scrollTop + 5) {
-                $this.css('paddingTop', 44).find('.meta').css({
-                    position: 'fixed',
-                    top: 5,
-                    borderTop: '1px solid #d8d8d8',
-                    width: $this.width()
-                });
+                $this.addClass('userscript-dsf-sticky');
+                $this.find('.meta').css({ width: $this.width() });
                 found = true;
             } else {
-                $this.css('paddingTop', 0).find('.meta').css({
-                    position: 'static',
-                    top: 0,
-                    borderTop: 'none',
-                    width: 'auto'
-                });
+                $this.removeClass('userscript-dsf-sticky');
+                $this.find('.meta').css({ width: 'auto' });
             }
         });
     });
